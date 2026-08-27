9dfdc0cf60261b0fbaa147cf3702ac5725e53b2b
Rodrigo Castro <124915528+rmacastro@users.noreply.github.com>
2026-08-27 02:57:20 +0000

    fix error message in migrations/index.js

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,8 +5,8 @@ import database from "infra/database.js"
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
-    return response.status(405).end({
-      error: `Method ${request.method} Not Allowed`
+    return response.status(405).json({
+      error: `Method "${request.method}" Not Allowed`,
     });
   }
 
@@ -15,33 +15,33 @@ export default async function migrations(request, response) {
   try {
   dbClient = await database.getNewClient();
 
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  }
-
-
-  if(request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if(request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    })
-    
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+
+    if(request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions)
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if(request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      })
+      
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
 
   } catch (error) {
     console.error(error);
